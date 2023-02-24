51f5bdcde94689b80414d9d2a0a4f3bf5386f0d0
Talha <mdtalha2008@gmail.com>
2023-02-25 02:03:23 +0600

    about update

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg';
 
 const Login = () => {
@@ -13,20 +14,20 @@ const Login = () => {
       
      <img className= ''src={img} alt="" />
     </div>
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
       <from onSubmit={handleLogin} className="card-body">
       <h1 className="text-5xl text-center font-bold">Login now</h1>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
+          <input type="text" name='email' placeholder="email" className="input input-bordered" />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
+          <input type="text" name='password' placeholder="password" className="input input-bordered" />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
@@ -35,6 +36,7 @@ const Login = () => {
             <input className='btn btn-primary' type="submit" value="Login" />
         </div>
       </from>
+      <p className='text-center'>New to EyeGlass <Link className='text-orange-800 font-bold' to='/signup'>Sign Up</Link></p>
     </div>
   </div>
 </div>
